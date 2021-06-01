a3069653171dc852e5800320f5c0883dbc4d1f30
zhutao <zhutao@wepie.com>
2021-06-01 21:16:57 +0800

    test

---
+++ Runtime/ProfileTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfileTest : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        TestCase1();
+    }
+
+    void TestCase1()
+    {
+        int t = 20000000;
+        Debug.Log("=====");
+        System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+        sw.Start();
+        for (int i = 0; i < t; i++)
+        {
+            int m = 8;
+            m = m << 15;
+            //Debug.Log(m);
+            m = m >> 15;
+            //Debug.Log(m);
+        }
+        Debug.Log("test tims is :" + sw.ElapsedMilliseconds);
+        sw.Restart();
+        for (int i = 0; i < t; i++)
+        {
+            int m = 8;
+            m = m * 10000;
+            //Debug.Log(m);
+            m = m / 10000;
+            //Debug.Log(m);
+        }
+
+        Debug.Log("test2 tims is :" + sw.ElapsedMilliseconds);
+    }
+}
